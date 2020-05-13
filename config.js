f364a18eae3c80b8555a652fc86109bca70eb81e
王芸 <wangyun.a@admaster.com.cn>
2020-05-13 23:22:52 +0800

    init

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: '我的技术学习记录',
+  subtitle: 'Web developer, Indie Hacker, Geek.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'wing wang',
+    photo: '/photo.jpg',
+    bio: 'Web developer, Indie Hacker, Geek.',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
---
+++ src/components/Page/Page.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react';
+import styles from './Page.module.scss';
+
+type Props = {
+  title?: string,
+  children: React.Node
+};
+
+const Page = ({ title, children }: Props) => {
+  const pageRef = useRef();
+
+  useEffect(() => {
+    pageRef.current.scrollIntoView();
+  });
+
+  return (
+    <div ref={pageRef} className={styles['page']}>
+      <div className={styles['page__inner']}>
+        { title && <h1 className={styles['page__title']}>{title}</h1>}
+        <div className={styles['page__body']}>
+          {children}
+        </div>
+        <div className={styles.footer}>蜀ICP备19033202号-1</div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
