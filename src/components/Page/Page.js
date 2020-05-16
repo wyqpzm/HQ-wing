20a38a282afae97e608603f6945bd0a81038dcf1
王芸 <wangyun.a@admaster.com.cn>
2020-05-16 23:57:40 +0800

    备案信息修改

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,9 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import styles from './Page.module.scss';
+import React, { useRef, useEffect } from "react";
+import styles from "./Page.module.scss";
 
 type Props = {
   title?: string,
-  children: React.Node
+  children: React.Node,
 };
 
 const Page = ({ title, children }: Props) => {
@@ -14,13 +14,13 @@ const Page = ({ title, children }: Props) => {
   });
 
   return (
-    <div ref={pageRef} className={styles['page']}>
-      <div className={styles['page__inner']}>
-        { title && <h1 className={styles['page__title']}>{title}</h1>}
-        <div className={styles['page__body']}>
-          {children}
+    <div ref={pageRef} className={styles["page"]}>
+      <div className={styles["page__inner"]}>
+        {title && <h1 className={styles["page__title"]}>{title}</h1>}
+        <div className={styles["page__body"]}>{children}</div>
+        <div className={styles.footer}>
+          <a href="http://beian.miit.gov.cn">蜀ICP备19033202号-1</a>
         </div>
-        <div className={styles.footer}>蜀ICP备19033202号-1</div>
       </div>
     </div>
   );
